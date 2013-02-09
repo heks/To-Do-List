a64a58c5bb54f383ebc0f91617ad6d529450bb9e
Daniel <daniel.rasinski@yahoo.com>
2013-02-09 00:50:19 -0600

    added x-out

--- custom.js
+++ custom.js
@@ -1,9 +1,26 @@
 $(document).ready(function() {
 
 
+
+
 	$("#add_new_item").click(function() {
+
     	var x = $("#new_item").val() 
+
+    	var test = $('<button/>',
+    	{
+        	text: 'Remove',
+        	click: function () { alert('hi'); }
+    	});
+
+    	//var y = $("<input type="button" id="remove_item" value="Remove" </button>").val(); 
     	$("<li>" + x + "</li>").appendTo("#mylist")
+
+
+	});
+ 
+	$('li').live('click',function() {
+		$(this).css("text-decoration","line-through");
 	});
 
 
