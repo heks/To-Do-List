5366882a065f20ac660fe49e8d5874a5df528ad2
Daniel <daniel.rasinski@yahoo.com>
2013-02-10 14:33:53 -0600

    added 'enter' keystroke to add to the list

--- custom.js
+++ custom.js
@@ -1,13 +1,9 @@
-function remove_list_item()
-{
-	if ($(".remove_me").is(':animated')) return false;
-		//var x = $('#mylist').filter('text-decoration','line-through')
-		//setTimeout(function () {
-			$(".remove_me").fadeTo(1000,0.1).animate({height:'toggle'},"slow",function(){
-				$('.remove_me').remove()});
-}
+
+
+
 
 $(document).ready(function() {
+
 	$("#add_new_item").click(function() {
 
 		if( $(":input").val() == "" ) {
@@ -16,22 +12,35 @@ $(document).ready(function() {
 
     	var x = "<li>";
     	x +=  $("#new_item").val();
-    	x += "<button id=\"delete\">X</button>";
+    	x += "<button id=\"delete\">x</button>";
+    	x += "<button id=\"add\">+</button>";
     	x += "</li>";	
     	$(x).fadeIn("slow").appendTo("#mylist")
-
-//.attr("id","list_item" + i++)
-
-    	//var y = $('#new_item').append('<input type="button" id="remove_item" value="My button">').button() // Add a click handler
-		//$('#selector') // Replace this selector with one suitable for you
-		//.append('<input type="button" value="My button">') // Create the element
-		//.button() // Ask jQuery UI to buttonize it
-		//.click(function(){ alert('I was clicked!');}); // Add a click handler
-
-    	//var y = $("<input type="button" id="remove_item" value="Remove" </button>").val(); 
-    	//$(x).fadeIn("slow").appendTo("#mylist")
 	});
 
+	$('#new_item').bind('keypress', function(e) {
+		if(e.keyCode==13){
+			var x = "<li>";
+    		x +=  $("#new_item").val();
+    		x += "<button id=\"delete\">x</button>";
+    		x += "<button id=\"add\">+</button>";
+    		x += "</li>";	
+    		$(x).fadeIn("slow").appendTo("#mylist")
+		}
+});
+
+/*
+	$("#add").live('click',function() {
+		var x = "<ul>";
+		x += "<li>"
+    	x +=  $("#new_item").val();
+    	x += "<button id=\"delete\">x</button>";
+    	x += "<button id=\"add\">+</button>";
+    	x += "</li>"
+    	x += "</ul>";	
+    	$(x).fadeIn("slow").appendTo($(this).parent());
+	});
+*/
 	$('#delete').live('click',function() {
 		$(this).parent().fadeTo(1000,0.1).animate({height:'toggle'},"slow",function(){
 				$(this).remove()
@@ -55,16 +64,9 @@ $(document).ready(function() {
 
 	$("#remove_all_items").bind('click',function() {
 		if ($(".remove_me").is(':animated')) return false;
-		//var x = $('#mylist').filter('text-decoration','line-through')
-		//setTimeout(function () {
 			$(".remove_me").fadeTo(1000,0.1).animate({height:'toggle'},"slow",function(){
 				$('.remove_me').remove()
 			});
-				//},500)
-
-		//setTimeout(function() {
-		//	$('.remove_me').remove(); }, 3000)
-		
 	});
 
 })
